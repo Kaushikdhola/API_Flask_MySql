513d979ebf1850b0a8a7a0960780431c5450cc19
Kaushikdhola <Kaushikdhola02@gmail.com>
2022-02-09 11:25:23 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# Data fetching and storing with Python Flask and Mysql
+
+This assignment is divided into two parts:
+
+- Create an API which will pass all the details from orders table to web page in a representational view.
+- Write a python script to retrive records from the past one hour and store it in the excel file. After that send this excel file as an attachment to particular email id.
+
+Orders table contains following columns:
+```
+    productID 
+    product_name
+    product_price
+    quantity
+    total_cost
+    order_date_time
+```
+- To get started, you have to execute the **orders.sql file** to create the table and insert particular data in the table.
+
+## File Structure
+- Main.Py
+- Templates -->
+   ```
+     1. Index.Html
+     2. One_hour.html
+   ```
+- Excel_file -->
+   ```     
+     orders_data.csv
+   ```
+- orders.sql
+
+## Prerequisites
+- Python
+- Mysql
+- Flask
+- Stable internet connection 
+
+## Installation
+
+Install following libraries with python.
+```python
+pip install mysql
+pip install flask
+pip install mimetypes
+pip install email
+pip install smtplib
+pip install pandas
+```
+
+## How to run the project
+
+Note: if you want to run the project in local system then some code changes required in database connection arguments (such as db username, password) and also changes in local path to store and retrive excel file.
+
+Post all the required changes go to project directory.
+
+Execute the Main.py file.
+ 
+ ```python     
+     py Main.py
+ ```
+
+You will be able to see a **index.html** web page containing order table data in a tabular format and below that **View Past One hour Data button**. 
+
+The index.html web page shows all the data stored inside the orders table. 
+
+If you want to see the data stored in past one  hour then click on the **View Past One hour Data button**. 
+
+By clicking on the button it will redirect you to **One_hour.html** web page that contains orders table data that are updated in last one hour in the tabular format.
+
+Simultaneously this one hour orders table data are stored in excel file (.csv) and sent as an attachment to specified email address(email address is specified in the code). 
---
+++ main.py
@@ -0,0 +1,110 @@
+import mimetypes
+from flask import Flask, render_template
+import mysql.connector
+import pandas as pd
+import smtplib
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+try:
+
+    #establishing connection to mysql server and database 
+    db = mysql.connector.connect(host='localhost', user='root', password='kaushik12', database='oneture')
+    print("Connected to database")
+except:
+    print("unable to connect to the database")
+
+cursor = db.cursor()
+subject = "Data from Sql table to Excel file"
+text = "This email is contains attached excel file that consist of data fetched from sql database."
+send_to = "kaushikdhola02@gmail.com"
+send_from = "kdhola1999@gmail.com"
+password = "Kd1234567"
+
+app = Flask(__name__)
+
+#Fetches all the records from order table and display it in the index.html web page
+@app.route('/', methods=['GET'])
+def index():
+    
+    #Executes select query to retrive all the records
+    cursor.execute("SELECT * FROM orders")
+    data = cursor.fetchall()
+
+    #sends all the records to index.html web page using data object 
+    return render_template('index.html', data=data)
+
+#Fetches records from order table updated in last one hour and display it in the One_hour.html web page
+
+@app.route('/One_hour', methods=['GET'])
+def One_hour():
+
+    #Executes select query to retrive records updated in last one hour
+    cursor.execute("select *from orders where  order_date_time >= now() - interval 1 hour")
+    data = cursor.fetchall()
+    
+    #calls  SqlData_to_Excelfile function to store data into excel file
+    SqlData_to_Excelfile(data)
+
+    #sends  fetched records to One_hour.html web page using data object 
+    return render_template('One_hour.html', data=data)
+
+
+#Data updated in last one hour are stored in the excel file
+
+def SqlData_to_Excelfile(data):
+    
+    #store data into dataframe object derived from pandas library
+    df = pd.DataFrame(data)
+
+    #convert dataframe to csv file
+    df.to_csv(r'F:\Oneture\Project\Excel_file\orders_data.csv', index=False)
+    Send_mail()
+
+#Excel file send as an attachment to the specified email address
+
+def Send_mail():
+
+    emailfrom = "kdhola1999@gmail.com"
+    emailto = "kaushikdhola02@gmail.com"
+    fileToSend = r"F:\Oneture\Project\Excel_file\orders_data.csv"
+    username = "kdhola1999@gmail.com"
+    password = "Kd1234567"
+
+    msg = MIMEMultipart()
+    msg["From"] = emailfrom
+    msg["To"] = emailto
+    msg["Subject"] = "Data from Sql table to Excel file"
+    msg.preamble = "This email is contains attached excel file that consist of data fetched from sql database."
+
+    ctype, encoding = mimetypes.guess_type(fileToSend)
+    if ctype is None or encoding is not None:
+        ctype = "application/octet-stream"
+
+    maintype, subtype = ctype.split("/", 1)
+
+    if maintype == "text":
+        fp = open(fileToSend)
+        # Note: we should handle calculating the charset
+        attachment = MIMEText(fp.read(), _subtype=subtype)
+        fp.close()
+    else:
+        fp = open(fileToSend, "rb")
+        attachment = MIMEBase(maintype, subtype)
+        attachment.set_payload(fp.read())
+        fp.close()
+        encoders.encode_base64(attachment)
+    attachment.add_header("Content-Disposition", "attachment", filename="orders_data.csv")
+    msg.attach(attachment)
+
+    server = smtplib.SMTP("smtp.gmail.com:587")
+    server.starttls()
+    server.login(username, password)
+    server.sendmail(emailfrom, emailto, msg.as_string())
+    server.quit()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
